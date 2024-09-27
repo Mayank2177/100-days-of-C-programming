386a4d2ec2cf5ea1a1afe5d3ab899ce10a479be3
Mayank2177 <153661521+Mayank2177@users.noreply.github.com>
2024-09-28 01:57:03 +0530

    Create double_last_digit.c

---
+++ Playing with Numbers and Digits/double_last_digit.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main(){
+/* Take input from user*/
+  int num,remainder,Final_number;
+  scanf("%d", &num);
+  remainder= num%10;
+  Final_number= num + remainder;
+  printf("%d %d", remainder, Final_number);
+
+  return 0;
+}
