a70e37c6dfd6f47e25796a25b71d407c24bb0c91
Mayank2177 <153661521+Mayank2177@users.noreply.github.com>
2024-11-22 15:24:57 +0530

    Update Palindrom_Number.c

--- Solving Mathematical Expressions/Palindrom_Number.c
+++ Solving Mathematical Expressions/Palindrom_Number.c
@@ -1,1 +1,33 @@
+include <stdio.h>
 
+int main(){
+
+  #include <stdio.h>
+
+int main() {
+   int n;
+   printf("Enter the digits of n: ");
+   scanf("%d", &n);
+   
+   int i,a[100];
+   printf("Enter the number: ");
+    for(int i=0; i<n;i++)
+        scanf("%d", a[i]);
+        
+    printf("Given number: ");
+    for(int i=0; i<n;i++)
+        printf("%d", a[i]);
+     
+    for(int i=0; i<n;i++)
+        int remainder= n/(10 ** (n-i);
+        
+        printf();
+        
+    return 0;
+  
+  /* Given an integer x, return true if x is a 
+palindrome
+, and false otherwise. */
+
+
+}
