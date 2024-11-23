20d280bbc2f8ccf447c1a39861ceed76450bfeb4
Mayank2177 <153661521+Mayank2177@users.noreply.github.com>
2024-11-23 17:43:03 +0530

    Update Leader in a array.c

--- Problems on Arrays/Leader in a array.c
+++ Problems on Arrays/Leader in a array.c
@@ -1,1 +1,21 @@
+#include <stdio.h>
 
+int main() {
+    
+    int n,a[100];
+    
+    printf("Enter the length of array: ");
+    scanf("%d", &n);
+    
+    int i;
+    printf("Enter the array: ");
+    for(i=0;i<n;i++)
+        scanf("%d", &a[i]);
+    
+    printf("\nLeagers of array are: \n");
+    for(i=0;i<n;i++)
+        if(a[i]>a[i+1] && a[i]>a[i-1])
+            
+            printf("%d\n", a[i]);
+            
+    return 0;
