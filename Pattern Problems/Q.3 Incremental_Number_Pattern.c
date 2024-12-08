64d6f8e5bc8648508f572a5b1e4c7aa1ba7d1d5d
Mayank2177 <153661521+Mayank2177@users.noreply.github.com>
2024-12-08 14:40:11 +0530

    Update and rename Q.3 Incremental Number Pattern.c to Q.3 Incremental_Number_Pattern.c

---
+++ Pattern Problems/Q.3 Incremental_Number_Pattern.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main(){
+  int n;
+  
+  printf("Enter the height of the pattern: ");
+  scanf("%d", &n);
+  
+
+  for(i=0;i<n;i++)
+    for(j=0;j<=i;j++)
+      int a=1;
+      printf("a");
+      a=a+1;
+    printf("\n");
+
+  return 0;
+}
